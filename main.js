be1f340c0b446dc2625a75635e33ad7c70da2e9e
Ningdan <49057216+ningdandan@users.noreply.github.com>
2021-06-12 22:02:21 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var scene = document.getElementById('js-scene');
+var parallax = new Parallax(scene);
+// var parallax = new Parallax(scene, {
+//     hoverOnly: true,
+//     relativeInput: true,
+//     inputElement: input
+//   });
